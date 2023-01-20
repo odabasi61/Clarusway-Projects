f26e38822aa1b15c5166b8763d79d482b93a908e
odabasi61 <aliodabas_a@hotmail.com>
2023-01-20 20:05:21 +0300

    remove added

--- Checkout Page/script.js
+++ Checkout Page/script.js
@@ -1,21 +1,28 @@
 "use script";
 
 // Buttons
-let container = document.querySelector(".container");
-let plus = document.querySelectorAll(".plus");
-let minus = document.querySelectorAll(".minus");
-let remove = document.querySelectorAll(".remove");
+const container = document.querySelector(".container");
+// let plus = document.querySelectorAll(".plus");
+// let minus = document.querySelectorAll(".minus");
+// let remove = document.querySelectorAll(".remove");
 
 // Amount
 // let quantity = document.querySelectorAll("#quantity");
 // let stotal = document.querySelectorAll(".stotal");
 
 // Totals
-let subt = document.querySelector(".subt");
-let tax = document.querySelector(".tax");
-let ship = document.querySelector(".ship");
-let total = document.querySelector(".total");
+// let subt = document.querySelector(".subt");
+// let tax = document.querySelector(".tax");
+// let ship = document.querySelector(".ship");
+// let total = document.querySelector(".total");
+
+const taxRate = 0.18;
+
 let mltply;
+
+window.addEventListener("load", () => {
+  accountTotal();
+});
 
 // plus.forEach((p) => {
 //   p.addEventListener("click", (e) => {
@@ -48,6 +55,10 @@ container.addEventListener("click", function (e) {
     e.target.previousElementSibling.innerText++;
     account(e.target.closest(".info"));
   }
+  else if (e.target.classList.contains("remove")) {
+    e.target.closest(".product").remove();
+    accountTotal();
+  }
 });
 
 let account = (a) => {
@@ -55,6 +66,22 @@ let account = (a) => {
   let quantity = a.querySelector("#quantity").innerText;
   let result = +(price * quantity).toFixed(2);
   a.querySelector(".stotal").innerText = result;
+  accountTotal();
 };
 
-let accountTotal = () => {};
+let accountTotal = () => {
+  let allStotal = document.querySelectorAll('.stotal');
+  let sumOfSubTotal = 0;
+  allStotal.forEach((sttls) => {
+    sumOfSubTotal += +sttls.innerText;
+  });
+
+  let taxCost = sumOfSubTotal * tax;
+
+  let shippingCost = sumOfSubTotal > 20 ? 15.00 : 0;
+
+  document.querySelector('.subt').innerText = sumOfSubTotal.toFixed(2);
+  document.querySelector('.tax').innerText = taxCost.toFixed(2);
+  document.querySelector('.ship').innerText = shippingCost.toFixed(2);
+  document.querySelector('.total').innerText = (sumOfSubTotal + taxCost + shippingCost).toFixed(2);
+};
