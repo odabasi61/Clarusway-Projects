1ffa734727ffd78c97898e3b03dba007a75f490a
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-01-20 18:07:42 +0300

    some buttons are functioned

--- Checkout Page/script.js
+++ Checkout Page/script.js
@@ -1,1 +1,60 @@
 "use script";
+
+// Buttons
+let container = document.querySelector(".container");
+let plus = document.querySelectorAll(".plus");
+let minus = document.querySelectorAll(".minus");
+let remove = document.querySelectorAll(".remove");
+
+// Amount
+// let quantity = document.querySelectorAll("#quantity");
+// let stotal = document.querySelectorAll(".stotal");
+
+// Totals
+let subt = document.querySelector(".subt");
+let tax = document.querySelector(".tax");
+let ship = document.querySelector(".ship");
+let total = document.querySelector(".total");
+let mltply;
+
+// plus.forEach((p) => {
+//   p.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     ++p.previousElementSibling.innerText;
+//     let price = +p.parentElement.previousElementSibling.children[1].textContent;
+//     mltply = +p.parentElement.children[1].textContent * price;
+//     stotal = mltply.toFixed(2);
+//     p.parentElement.nextElementSibling.nextElementSibling.children[0].textContent = `${stotal}`;
+//   });
+// });
+
+// minus.forEach((p) => {
+//   p.addEventListener("click", (e) => {
+//     if (p.nextElementSibling.innerText > 1) {
+//       e.preventDefault();
+//       --p.nextElementSibling.innerText;
+//     }
+//   });
+// });
+
+container.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("minus")) {
+    if (e.target.nextElementSibling.innerText > 1) {
+      e.target.nextElementSibling.innerText--;
+      account(e.target.closest(".info"));
+    }
+  } else if (e.target.classList.contains("plus")) {
+    e.target.previousElementSibling.innerText++;
+    account(e.target.closest(".info"));
+  }
+});
+
+let account = (a) => {
+  let price = a.querySelector(".span1").innerText;
+  let quantity = a.querySelector("#quantity").innerText;
+  let result = +(price * quantity).toFixed(2);
+  a.querySelector(".stotal").innerText = result;
+};
+
+let accountTotal = () => {};
