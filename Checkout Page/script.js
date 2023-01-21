737e900d3fb8411f3d0871a4112f9d99d5a26c2f
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-01-21 09:25:20 +0300

    finished

--- Checkout Page/script.js
+++ Checkout Page/script.js
@@ -1,61 +1,24 @@
 "use script";
 
-// Buttons
 const container = document.querySelector(".container");
-// let plus = document.querySelectorAll(".plus");
-// let minus = document.querySelectorAll(".minus");
-// let remove = document.querySelectorAll(".remove");
-
-// Amount
-// let quantity = document.querySelectorAll("#quantity");
-// let stotal = document.querySelectorAll(".stotal");
-
-// Totals
-// let subt = document.querySelector(".subt");
-// let tax = document.querySelector(".tax");
-// let ship = document.querySelector(".ship");
-// let total = document.querySelector(".total");
-
 const taxRate = 0.18;
-
 let mltply;
 
 window.addEventListener("load", () => {
   accountTotal();
 });
 
-// plus.forEach((p) => {
-//   p.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     ++p.previousElementSibling.innerText;
-//     let price = +p.parentElement.previousElementSibling.children[1].textContent;
-//     mltply = +p.parentElement.children[1].textContent * price;
-//     stotal = mltply.toFixed(2);
-//     p.parentElement.nextElementSibling.nextElementSibling.children[0].textContent = `${stotal}`;
-//   });
-// });
-
-// minus.forEach((p) => {
-//   p.addEventListener("click", (e) => {
-//     if (p.nextElementSibling.innerText > 1) {
-//       e.preventDefault();
-//       --p.nextElementSibling.innerText;
-//     }
-//   });
-// });
-
 container.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.classList.contains("minus")) {
-    if (e.target.nextElementSibling.innerText > 1) {
+    if (e.target.nextElementSibling.innerText > 0) {
       e.target.nextElementSibling.innerText--;
       account(e.target.closest(".info"));
     }
   } else if (e.target.classList.contains("plus")) {
     e.target.previousElementSibling.innerText++;
     account(e.target.closest(".info"));
-  }
-  else if (e.target.classList.contains("remove")) {
+  } else if (e.target.classList.contains("remove")) {
     e.target.closest(".product").remove();
     accountTotal();
   }
@@ -70,18 +33,21 @@ let account = (a) => {
 };
 
 let accountTotal = () => {
-  let allStotal = document.querySelectorAll('.stotal');
+  let allStotal = document.querySelectorAll(".stotal");
   let sumOfSubTotal = 0;
   allStotal.forEach((sttls) => {
     sumOfSubTotal += +sttls.innerText;
   });
 
-  let taxCost = sumOfSubTotal * tax;
+  let taxCost = sumOfSubTotal * taxRate;
+  let shippingCost = sumOfSubTotal > 20 ? 15.0 : 0;
 
-  let shippingCost = sumOfSubTotal > 20 ? 15.00 : 0;
-
-  document.querySelector('.subt').innerText = sumOfSubTotal.toFixed(2);
-  document.querySelector('.tax').innerText = taxCost.toFixed(2);
-  document.querySelector('.ship').innerText = shippingCost.toFixed(2);
-  document.querySelector('.total').innerText = (sumOfSubTotal + taxCost + shippingCost).toFixed(2);
+  document.querySelector(".subt").innerText = sumOfSubTotal.toFixed(2);
+  document.querySelector(".tax").innerText = taxCost.toFixed(2);
+  document.querySelector(".ship").innerText = shippingCost.toFixed(2);
+  document.querySelector(".total").innerText = (
+    sumOfSubTotal +
+    taxCost +
+    shippingCost
+  ).toFixed(2);
 };
