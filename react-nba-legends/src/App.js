59b4c45b90315f18c6daf9ec0e06bc7b3fc00299
odabasi61 <aliodabas_a@hotmail.com>
2023-02-21 23:16:45 +0300

    nba project finished

--- react-nba-legends/src/App.js
+++ react-nba-legends/src/App.js
@@ -1,5 +1,13 @@
+import Container from "react-bootstrap/Container";
+import CardContainer from "./components/CardContainer";
+import Header from "./components/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="text-center mt-4">
+      <Header />
+      <CardContainer />
+    </Container>
+  );
 }
 
 export default App;
---
+++ react-nba-legends/src/components/Header.js
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import nbaLogo from "../assets/nba-logo.png";
+const Header = () => {
+  return (
+    <Container>
+      <Image src={nbaLogo}></Image>
+      <h1>NBA Legends</h1>
+    </Container>
+  );
+};
+export default Header;
